7b96eafce2465415ea3552e76feccd3d98102fb2
Shubham062004 <chaurasiaamrita400@gmail.com>
2024-03-02 09:04:57 +0530

    env files

--- Readme.md
+++ Readme.md
@@ -3,3 +3,5 @@ Title: Bench List
 Idea: Marking all the benches in my college
 
 Objective: You just search area name and you get where benches are located near you
+
+Deploy Link: https://s53-benchlist-server.onrender.com/ping
--- server.js
+++ server.js
@@ -1,16 +1,48 @@
 const express = require('express');
 const app = express();
 const port = process.env.PUBLIC_PORT || 6969;
+const {ConnectToDB,stopDatabase,isConnected} = require('./db')
 
-
-app.get("/ping",(req,res)=>{
-    res.send({ "message": "pong" })
+ConnectToDB().then((mongoConnection)=>{
+  app.get("/",(req,res)=>{
+      res.json({message : mongoConnection ? "connected" : "disconnected"})
+  })
 })
 
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`server running on PORT: ${port}`);
-  });
-}
 
-module.exports = app;
+app.listen(port,()=>{
+  console.log(`Server is running on http://localhost:${port}`);
+})
+
+
+
+
+
+// app.get("/ping",(req,res)=>{
+//     res.send({ "message": "pong" })
+// })
+// const startserver=async ()=>{
+//   await ConnectToDB();
+//   try {
+//     app.get("/",(req,res)=>{
+//       res.send(`database connection status: ${isConnected()? "connected": "not connected"}`)
+//     })
+//     app.listen(port, () => {
+//       // ConnectToDB()
+//       console.log(`server running on PORT: ${port}`);
+//     });
+//   } catch (error) {
+//     console.log("error", error)
+//   }
+  
+// }
+// startserver()
+
+// if (require.main === module) {
+//   app.listen(port, () => {
+//     // ConnectToDB()
+//     console.log(`server running on PORT: ${port}`);
+//   });
+// }
+
+
