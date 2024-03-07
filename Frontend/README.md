ad09d11a7e9ebd0a1e8bc15ab368778f9d1813b9
Shubham062004 <chaurasiaamrita400@gmail.com>
2024-03-07 16:13:01 +0530

    hello

--- Frontend/README.md
+++
@@ -1,1 +0,0 @@
-Deploy Link - https://benchlist.vercel.app/
--- Frontend/benchlist/src/components/DummyData.jsx
+++ Frontend/benchlist/src/components/DummyData.jsx
@@ -35,15 +35,15 @@ const benchList = () => {
                                 <div class="card">
                                     <div className="img">
                                         {/* <img src="https://th.bing.com/th/id/R.c533d613bcd759a8f1586600f135e90a?rik=9sz9wnyLtobccw&riu=http%3a%2f%2fwww.lpu.in%2facademics%2fimgs%2flive-projects%2ffull%2funi-mall.jpg&ehk=GWAf3Wq205PwwQzmwZfKu3kPI7jciJjpscHFa3za4js%3d&risl=&pid=ImgRaw&r=0" alt="" /> */}
-                                        <img src={elem.img} alt="" />
+                                        <img src={elem.image} alt="" />
                                     </div>
                                     <div className="location">
                                         <div className="locationEmoji"><img src="https://e7.pngegg.com/pngimages/457/630/png-clipart-location-logo-location-computer-icons-symbol-location-miscellaneous-angle-thumbnail.png" alt="" /></div>
-                                        <div className="text">{elem.placeName}</div>
+                                        <div className="text">{elem.placename}</div>
                                     </div>
                                     <div className="placename">
                                         <p className="text">
-                                            {elem.benchLocation}
+                                            {elem.location}
                                         </p>
                                     </div>
                                 </div>
